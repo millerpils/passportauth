d4510d436f4babd865dfc989b9afb6cf9abb9825
Miller <sbrn290@pfslap859648.enterprise.internal.city.ac.uk>
2019-03-15 09:39:42 +0000

    Passport auth with git

---
+++ index.js
@@ -0,0 +1,56 @@
+/*  EXPRESS SETUP  */
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => res.sendFile('auth.html', { root : __dirname}));
+
+const port = process.env.PORT || 3000;
+app.listen(port , () => console.log('App listening on port ' + port));
+
+
+
+/*  PASSPORT SETUP  */
+
+const passport = require('passport');
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.get('/success', (req, res) => res.send("You have successfully logged in"));
+app.get('/error', (req, res) => res.send("error logging in"));
+
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(obj, cb) {
+  cb(null, obj);
+});
+
+
+
+/*  GITHUB AUTH  */
+
+const GitHubStrategy = require('passport-github').Strategy;
+
+const GITHUB_CLIENT_ID = "Iv1.2c871e9b82527871"
+const GITHUB_CLIENT_SECRET = "f1022f3b56463900febf0b4484af52e66093924c";
+
+passport.use(new GitHubStrategy({
+    clientID: GITHUB_CLIENT_ID,
+    clientSecret: GITHUB_CLIENT_SECRET,
+    callbackURL: "/auth/github/callback"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+      return cb(null, profile);
+  }
+));
+
+app.get('/auth/github',
+  passport.authenticate('github'));
+
+app.get('/auth/github/callback',
+  passport.authenticate('github', { failureRedirect: '/error' }),
+  function(req, res) {
+    res.redirect('/success');
+  });
